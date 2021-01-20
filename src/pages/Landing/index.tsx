5c63e097783e2b9549df674dc49ddf110949e415
Xavi002 <diogomartins200214@gmail.com>
2021-01-20 22:23:54 +0000

    Initial commit

---
+++ src/pages/Landing/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, Header } from './styles';
+
+const Landing: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <Title>Dopamina</Title>
+      </Header>
+    </Container>
+  );
+};
+
+export default Landing;
---
+++ src/pages/Landing/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #181a20;
+`;
+
+export const Header = styled.View`
+margin-top: 50px;
+padding: 0 20px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 24px;
+  font-weight: 700;
+`;
